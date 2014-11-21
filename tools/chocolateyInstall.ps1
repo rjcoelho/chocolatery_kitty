efe8d690863ed7a7b855eba4e0fc1b1650b9ef0e
rjc <rui.coelho@altitude.com>
2014-11-21 17:47:20 +0000

    Initial release

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿# note: we cannot find public url to download from, so download them from http://www.9bis.net/kitty/?page=Download and put them in tools
+#$scriptPath = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+#$fileFullPath = Join-Path $scriptPath 'kitty.exe'
+#Get-ChocolateyWebFile 'kitty' "$fileFullPath" 'http://www.fosshub.com/download/kitty.exe'
+
+# note: already called by default
+#$installDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+#Get-ChocolateyBins $installDir
